1b2ec362e393f80f6d6149806b57a8a8229dc77b
Roman Stolyarchuk <roman@stolyarch.uk>
2024-10-30 20:39:19 +0400

    Implement CORS support and dynamic responses for well-known Matrix endpoints

--- src/index.js
+++ src/index.js
@@ -9,7 +9,23 @@
  */
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request, env) {
+		const headers = {
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "application/json;charset=UTF-8",
+		};
+
+		const path = new URL(request.url).pathname;
+		switch (path) {
+			case "/.well-known/matrix/client":
+				return new Response(
+					`{"m.homeserver": {"base_url": "${env.HOMESERVER_URL}"}, "m.identity_server": {"base_url": "${env.IDENTITY_SERVER_URL}"}}`,
+					{ headers: headers }
+				);
+			case "/.well-known/matrix/server":
+				return new Response(`{"m.server": "${env.FEDERATION_SERVER}"}`, { headers: headers });
+			default:
+				return new Response("Invalid request", { headers: headers });
+		}
 	},
 };
